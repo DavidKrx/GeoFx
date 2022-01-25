fae5e76d4558a9e16f75020b962f37e31edde4b5
David <davidcm097@gmail.com>
2022-01-25 10:21:02 +0000

    Falta tareas en segundo plano

--- src/main/java/dad/geo/App.java
+++ src/main/java/dad/geo/App.java
@@ -3,6 +3,8 @@ package dad.geo;
 import dad.geo.controller.MainController;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class App extends Application{
@@ -19,4 +21,18 @@ public class App extends Application{
 	public static void main(String[] args) {
 		launch(args);
 	}
+   
+	public static void confirm(String header, String headerContent) {
+		Alert alerta=new Alert(AlertType.INFORMATION);
+		alerta.setTitle(header);
+		alerta.setHeaderText(headerContent);
+		alerta.showAndWait();
+	}
+    public static void reject(String titulo,String header, String content) {
+  		Alert alerta=new Alert(AlertType.ERROR);
+  		alerta.setTitle(titulo);
+  		alerta.setHeaderText(header);
+  		alerta.setContentText(content);
+  		alerta.showAndWait();
+  	}
 }
--- src/main/java/dad/geo/controller/MainController.java
+++ src/main/java/dad/geo/controller/MainController.java
@@ -2,11 +2,7 @@ package dad.geo.controller;
 	import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import dad.geo.api.GeoService;
-import dad.geo.model.All;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
@@ -14,8 +10,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-	import javafx.scene.control.Tab;
-	import javafx.scene.control.TextField;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 
@@ -46,46 +42,42 @@ import javafx.scene.layout.BorderPane;
 
 	    @FXML
 	    void OncheckIpAction(ActionEvent event) throws Exception {
-	    
-	    	//Location
-	    	locat.setLatitudPro(geoService.listBreeds(ip.getValue()).getLatitude().toString());
-	    	locat.setLongitudPro(geoService.listBreeds(ip.getValue()).getLongitude().toString());
-	    	
-	    	locat.setLocationPro(geoService.listBreeds(ip.getValue()).getCountryName().concat(" ("
-	    			.concat(geoService.listBreeds(ip.getValue()).getCountryCode())+")"));
-	    	locat.setImagePro(new Image("/images/96x64/"+geoService.listBreeds(ip.getValue()).getCountryCode().concat(".png")));
-	    	System.out.print(geoService.listBreeds(ip.getValue()).getCountryCode());
-	    	locat.setCityPro(geoService.listBreeds(ip.getValue()).getCity().toString().concat("(".concat(geoService.listBreeds(ip.getValue()).getRegionName())+")"));
-	    	
-	    	locat.setZipCodePro(geoService.listBreeds(ip.getValue()).getZip().toString());
-	    	
-	    	locat.setLanguagePro(geoService.listBreeds(ip.getValue()).getLocation().getLanguages().get(0).getName().concat("(".concat(
-	    			geoService.listBreeds(ip.getValue()).getLocation().getLanguages().get(0).getCode())).concat(")"));
-	    	
-	    	locat.setTimezonePro(geoService.listBreeds(ip.getValue()).getTimeZone().getCode());
-	    	locat.setCallingCodePro("+".concat(geoService.listBreeds(ip.getValue()).getLocation().getCallingCode()));
-	    	locat.setCurrencyPro(geoService.listBreeds(ip.getValue()).getCurrency().getName().concat("(".concat(geoService.listBreeds(ip.getValue()).getCurrency().getSymbol())+")"));
-	    	
-	    	//Connection
-	    	connect.setIpPro(geoService.listBreeds(ip.getValue()).getIp());
-	    	connect.setIspPro(geoService.listBreeds(ip.getValue()).getConnection().getIsp());
-	    	connect.setTypePro(geoService.listBreeds(ip.getValue()).getType());
-	    	connect.setAsnPro(geoService.listBreeds(ip.getValue()).getConnection().getAsn().toString());
-	    	connect.setHostnamePro(geoService.listBreeds(ip.getValue()).getHostname());
-	    	//Secury
-	    	secury.setProxyPro(geoService.listBreeds(ip.getValue()).getSecurity().getIsProxy());
-	    	secury.setTorPro(geoService.listBreeds(ip.getValue()).getSecurity().getIsTor());
-	    	secury.setCrawlerPro(geoService.listBreeds(ip.getValue()).getSecurity().getIsCrawler());
-	    	secury.setThreatLevelPro(geoService.listBreeds(ip.getValue()).getSecurity().getThreatLevel());
-	   
-	    			if(geoService.listBreeds(ip.getValue()).getSecurity().getThreatTypes()==null) {
-	    			 	secury.setSecurityPro("This IP is safe. No threats have been detected.");
-	    				secury.setPotentialTypesPro("No threats have been detected for this IP address.");
-	    			}else {
-	    				secury.setSecurityPro("This IP isnt safe. Threats have been detected.");
-	    				secury.setPotentialTypesPro("Threats have been detected for this IP address. Carefull!");
-	    			}		
-	    }
+					//Location
+			    	locat.setLatitudPro(geoService.listBreeds(ip.getValue()).getLatitude().toString());
+			    	locat.setLongitudPro(geoService.listBreeds(ip.getValue()).getLongitude().toString());
+			    	locat.setLocationPro(geoService.listBreeds(ip.getValue()).getCountryName().concat(" ("
+			    			.concat(geoService.listBreeds(ip.getValue()).getCountryCode())+")"));
+			    	locat.setImagePro(new Image("/images/96x64/"+geoService.listBreeds(ip.getValue()).getCountryCode().concat(".png")));
+			    	locat.setCityPro(geoService.listBreeds(ip.getValue()).getCity().toString().concat("("
+			    			.concat(geoService.listBreeds(ip.getValue()).getRegionName())+")"));
+			    	locat.setZipCodePro(geoService.listBreeds(ip.getValue()).getZip().toString());
+			    	locat.setLanguagePro(geoService.listBreeds(ip.getValue()).getLocation().getLanguages().get(0).getName().concat("(".concat(
+			    			geoService.listBreeds(ip.getValue()).getLocation().getLanguages().get(0).getCode())).concat(")"));
+			    	locat.setTimezonePro(geoService.listBreeds(ip.getValue()).getTimeZone().getCode());
+			    	locat.setCallingCodePro("+".concat(geoService.listBreeds(ip.getValue()).getLocation().getCallingCode()));
+			    	locat.setCurrencyPro(geoService.listBreeds(ip.getValue()).getCurrency().getName().concat("("
+			    			.concat(geoService.listBreeds(ip.getValue()).getCurrency().getSymbol())+")"));
+			    	
+			    	//Connection
+			    	connect.setIpPro(geoService.listBreeds(ip.getValue()).getIp());
+			    	connect.setIspPro(geoService.listBreeds(ip.getValue()).getConnection().getIsp());
+			    	connect.setTypePro(geoService.listBreeds(ip.getValue()).getType());
+			    	connect.setAsnPro(geoService.listBreeds(ip.getValue()).getConnection().getAsn().toString());
+			    	connect.setHostnamePro(geoService.listBreeds(ip.getValue()).getHostname());
+			    	//Secury
+			    	secury.setProxyPro(geoService.listBreeds(ip.getValue()).getSecurity().getIsProxy());
+			    	secury.setTorPro(geoService.listBreeds(ip.getValue()).getSecurity().getIsTor());
+			    	secury.setCrawlerPro(geoService.listBreeds(ip.getValue()).getSecurity().getIsCrawler());
+			    	secury.setThreatLevelPro(geoService.listBreeds(ip.getValue()).getSecurity().getThreatLevel());
+			   
+			    			if(geoService.listBreeds(ip.getValue()).getSecurity().getThreatTypes()==null) {
+			    			 	secury.setSecurityPro("This IP is safe. No threats have been detected.");
+			    				secury.setPotentialTypesPro("No threats have been detected for this IP address.");
+			    			}else {
+			    				secury.setSecurityPro("This IP isnt safe. Threats have been detected.");
+			    				secury.setPotentialTypesPro("Threats have been detected for this IP address. Carefull!");
+			    			}		
+	    	}
 	    	public void initialize(URL location, ResourceBundle resources) {
 			ip.bind(IPTextfield.textProperty());
 			
@@ -95,7 +87,6 @@ import javafx.scene.layout.BorderPane;
 		}
 		 public MainController() throws IOException {
 			// TODO Auto-generated constructor stub
-		
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainView.fxml"));
 			loader.setController(this);
 			loader.load();
